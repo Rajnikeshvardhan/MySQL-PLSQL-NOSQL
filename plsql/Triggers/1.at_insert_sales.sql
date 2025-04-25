cad49cae3562f633b2cf838da40009288c176400
RajNikeshVardhan <nikesh9701@gmail.com>
2025-04-26 02:05:42 +0530

    Delete plsql/Triggers/1.at_insert_sales.sql

--- plsql/Triggers/1.at_insert_sales.sql
+++
@@ -1,8 +0,0 @@
-delimiter //
-drop trigger if exists at_insert_sales//
-create trigger at_insert_sales after insert on sales for each row
-begin
-    update products set quantity=quantity-new.quantity_sold where product_id= new.product_id;
-end //
-delimiter ;
-
